7b2834cc8d179a7fa32e287fdb0d746aee865f8b
Thomas Kershaw <tom@kershaw.org>
2026-02-20 12:55:15 +0000

    Add splitter cases

--- protocol_visualiser.py
+++ protocol_visualiser.py
@@ -7,21 +7,7 @@ w = 50
 h = w
 r = h/2
 
-def process(protocol:str):
-    els = protocol.split()
-    for el in els:
-        match el:
-            case foo if foo in "ABCDE":
-                print("agent", foo)
-            case "->":
-                print("arrow")
-            case "-":
-                print("line")
-            case _:
-                print("?")
-    return els
-        
-assert process("A -> B") == ["A", "->", "B"]
+
 
 def draw_clock(t, x, y, w, h):
     t.fillcolor("#D0D0D0")
@@ -74,7 +60,8 @@ def draw(t=None, element="agent", ornament="", text="", x=0, y=0, w=100, h=100):
     
     if element == "connection":
         t.forward(w)
-        t.stamp()
+        if ornament == "arrow":
+            t.stamp()
     elif element == "agent":
         r=w/2
         t.teleport(x, y-r)
@@ -101,18 +88,40 @@ def draw(t=None, element="agent", ornament="", text="", x=0, y=0, w=100, h=100):
     elif ornament == "key":
         draw_key(t, x, y, w, h)
 
-
-
-
-draw(t, element='agent', ornament="key", text="Alice", x=x+w/2, y=y, w=w, h=h)
-x += w
-draw(t, element='connection', w=w)
-x += w
-draw(t, element='message', ornament="clock", text="a", x=x+w/2, y=y, w=w, h=h)
-x += w
-draw(t, element='connection', w=w)
-x += w
-draw(t, element='agent', text="Bob", x=x+w/2, y=y, w=w, h=h)
+def process(protocol:str, x=0, y=0, w=50, h=50):
+    els = protocol.split()
+    for el in els:
+        eltype = el[0]
+        if len(el) > 1:
+            elorn = el[1]
+        else:
+            elorn = ""
+        match eltype:
+            case eltype if eltype in "ABCDE" and elorn == "k":
+                print("agent", eltype)
+                draw(t, element='agent', ornament="key", text=eltype, x=x+w/2, y=y, w=w, h=h)
+            case eltype if eltype in "ABCDE":
+                print("agent", eltype)
+                draw(t, element='agent', text=eltype, x=x+w/2, y=y, w=w, h=h)
+            case "-" if elorn == ">":
+                print("arrow")
+                draw(t, element='connection', ornament="arrow", w=w)
+            case "-":
+                print("line")
+                draw(t, element='connection', w=w)
+            case "#" if elorn == "n":
+                print("nonce")
+                draw(t, element='message', ornament="clock", text="a", x=x+w/2, y=y, w=w, h=h)
+            case "#" :
+                print("message")
+                draw(t, element='message', text="a", x=x+w/2, y=y, w=w, h=h)
+            case _:
+                print("?")
+        x += w
+    return {"els":els, "x":x, "y":y}
+        
+assert process("A -> B")["els"] == ["A", "->", "B"]
+assert process("Ak - #n -> B", y=100)["els"] == ["Ak", "-", "#n", "->", "B"]
 
 t.hideturtle()
 turtle.update()
